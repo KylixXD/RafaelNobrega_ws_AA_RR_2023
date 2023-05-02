c8b6beb8f21cf06ac984d4c4613065a5fa96689b
Rafael Nobrega <rafaelnobregadelima2809@gmail.com>
2023-05-02 08:23:35 -0400

    first commit

---
+++ mergesort.c
@@ -0,0 +1,100 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+void mergeSort(int arr[], int l, int r);
+
+// Função de mesclagem usada pela função mergeSort()
+void merge(int arr[], int l, int m, int r) {
+    int i, j, k;
+    int n1 = m - l + 1;
+    int n2 = r - m;
+
+    // Cria arrays temporários
+    int L[n1], R[n2];
+    // Copia os dados para os arrays temporários L[] e R[]
+    for (i = 0; i < n1; i++)
+        L[i] = arr[l + i];
+    for (j = 0; j < n2; j++)
+        R[j] = arr[m + 1 + j];
+
+    // Mescla os arrays temporários de volta em arr[l..r]
+    i = 0; // Índice inicial do primeiro subarray
+    j = 0; // Índice inicial do segundo subarray
+    k = l; // Índice inicial do array mesclado
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j]) {
+            arr[k] = L[i];
+            i++;
+        }
+    else {
+        arr[k] = R[j];
+        j++;
+    }
+    k++;
+}
+
+// Copia os elementos restantes de L[], se houver algum
+while (i < n1) {
+    arr[k] = L[i];
+    i++;
+    k++;
+}
+
+// Copia os elementos restantes de R[], se houver algum
+while (j < n2) {
+    arr[k] = R[j];
+    j++;
+    k++;
+}
+}
+
+void mergeSort(int arr[], int l, int r) {
+    if (l < r) {
+        int m = l + (r - l) / 2; // Calcula o índice do elemento central
+        mergeSort(arr, l, m);
+        mergeSort(arr, m + 1, r);
+        merge(arr, l, m, r);
+    }
+}
+
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        printf("Erro: use %s <arquivo_de_entrada>\n", argv[0]);
+        return 1;
+    }
+
+    // Abre o arquivo de entrada
+    FILE *input_file = fopen(argv[1], "r");
+    if (input_file == NULL) {
+        printf("Erro: não foi possível abrir o arquivo de entrada\n");
+        return 1;
+    }
+
+    // Lê o número de elementos do arquivo de entrada
+    int num_elements;
+    fscanf(input_file, "%d", &num_elements);
+
+    // Lê os elementos do arquivo de entrada e armazena em um vetor
+    int *elements = malloc(num_elements * sizeof(int));
+    for (int i = 0; i < num_elements; i++) {
+        fscanf(input_file, "%d", &elements[i]);
+    }
+
+    // Fecha o arquivo de entrada
+    fclose(input_file);
+
+    // Mede o tempo de execução do Merge Sort
+    clock_t start_time = clock();
+    mergeSort(elements, 0, num_elements - 1);
+    clock_t end_time = clock();
+    double elapsed_time_ms = (double)(end_time - start_time) / (CLOCKS_PER_SEC / 1000);
+
+    // Imprime o tempo de execução do Merge Sort em milissegundos
+    printf(" %.2f \n", elapsed_time_ms);
+
+    // Libera a memória alocada
+    free(elements);
+
+    return 0;
+}
---
+++ script.py
@@ -0,0 +1,28 @@
+import subprocess
+import statistics
+import matplotlib.pyplot as plt
+import os
+
+input_files = ["./inputs/1kran.csv","./inputs/10kran.csv","./inputs/500kran.csv","./inputs/1_5mran.csv"]
+num_runs = 13
+
+mean_execution_times = []
+
+for input_file in input_files:
+    execution_times = []
+    for i in range(num_runs):
+        process = subprocess.Popen(["./mergesort", input_file], stdout=subprocess.PIPE)
+        output, error = process.communicate()
+        execution_time = float(output.decode("utf-8"))
+        execution_times.append(execution_time)
+    
+    mean_execution_time = statistics.mean(execution_times)
+    mean_execution_times.append(mean_execution_time)
+
+# Plotar o gráfico de barras
+input_files_names = [os.path.basename(file) for file in input_files]
+plt.plot(input_files_names, mean_execution_times)
+plt.xlabel("Arquivo de entrada")
+plt.ylabel("Tempo de execução médio (ms)")
+plt.title("Desempenho do Merge Sort para diferentes arquivos de entrada")
+plt.show()
